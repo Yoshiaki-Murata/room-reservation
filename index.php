f2105ee6e14a1ea3319900b1ba7d863deaf6d86e
Yoshiaki Murata <mrtysak.0517@gmail.com>
2026-03-26 23:59:46 +0900

    3/26終わり

--- index.php
+++ index.php
@@ -2,14 +2,11 @@
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>こんにちは</h1>
-</body>
-</html>
+<?php include __DIR__."./template/header.php"; ?>
+
+<main>
+
+
+</main>
+
+<?php include __DIR__."./template/footer.php"; ?>
